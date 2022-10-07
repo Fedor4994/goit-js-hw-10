009f054941f5b5b3d9f46c446d7bcf3f5a06d658
Fedor4994 <fedorsosnin04@gmail.com>
2022-10-07 14:12:35 +0300

    Make first taks

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButton.addEventListener('click', onStartButtonClick);
+
+function onStartButtonClick() {
+  const intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  stopButton.addEventListener('click', onStopButtonClick);
+  function onStopButtonClick() {
+    clearInterval(intervalId);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+  }
+}
