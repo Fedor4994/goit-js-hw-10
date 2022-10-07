c95d89c4e30a08ac318808ec6181f097297afaa6
Fedor4994 <fedorsosnin04@gmail.com>
2022-10-07 15:43:33 +0300

    First task refractorting

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,23 +1,25 @@
 const startButton = document.querySelector('button[data-start]');
 const stopButton = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
+let intervalId = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
 
 function onStartButtonClick() {
-  const intervalId = setInterval(() => {
+  intervalId = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
   startButton.disabled = true;
   stopButton.disabled = false;
-  stopButton.addEventListener('click', onStopButtonClick);
-  function onStopButtonClick() {
-    clearInterval(intervalId);
-    startButton.disabled = false;
-    stopButton.disabled = true;
-  }
+}
+
+function onStopButtonClick() {
+  clearInterval(intervalId);
+  startButton.disabled = false;
+  stopButton.disabled = true;
 }
