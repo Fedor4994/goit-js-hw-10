64f5573ae5ce86d5ad05e486f47f7374f1a92b6a
Fedor4994 <103943126+Fedor4994@users.noreply.github.com>
2022-10-07 11:13:05 +0000

    Deploying to gh-pages from @ Fedor4994/goit-js-hw-10@009f054941f5b5b3d9f46c446d7bcf3f5a06d658 🚀

---
+++ 01-color-switcher.b9580efa.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");t.addEventListener("click",(function(){const d=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3);t.disabled=!0,e.disabled=!1,e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}))}));
+//# sourceMappingURL=01-color-switcher.b9580efa.js.map
